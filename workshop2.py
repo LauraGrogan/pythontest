16d952a6476165a201d54bce2d9a0ec779109fde
Laura Grogan <LauraGrogan@users.noreply.github.com>
2021-08-19 14:53:37 +1000

    tried some functions

--- workshop2.py
+++ workshop2.py
@@ -63,6 +63,126 @@ for i in range(0,len(primes),1):
 for i in range(0,len(primes),1):
     print("the value of i in this loop is:",primes[i])
 
+# If statements
+mass=4.02
+mass=[4.02,2.0,3.0,3.1]
+if mass > 3.0:
+    print(mass, 'is large')
+else:
+    print(mass, 'is not large')
+    
+if mass <= 3.0 and mass > 10.0:
+    print(mass, 'is ridiculously large')
+else:
+    print(mass, 'is not large')
+
+# If, elif (else if) and else statements:
+if mass > 3.0 and mass <= 10.0:
+    print(mass,'is large')
+elif mass > 10.0:
+    print(mass,'is ridiculously large')
+else:
+    print(mass, 'is not large')
+    
+masses = [2.4,7.4,1.1,52.3] # containing a conditional statement inside a for loop
+for i in masses:
+    if i > 3.0 and i <= 10.0:
+        print(i,'is large')
+    elif i > 10.0:
+        print(i,'is ridiculously large')
+    else:
+        print(i, 'is not large')
+
+# could replace the 'and' with 'or' to allow either conditional statement to be true.
+
+masses=[2.4,7.4,1.1,52.3]
+velocity=[53.6,100.4,7.2,10000.5]
+if masses[1] > 3.0 and masses[2] <= 10.0 or velocity == 54:
+    print('all good')
+
+# glob library - deal with multiple files at once
+import glob
+import pandas as pd
+for filename in glob.glob('C:/Users/laura/Documents/20210818 Python workshop/data/*.csv'):
+    contents=pd.read_csv(filename)
+    print(filename,len(contents))
+
+contents = [] # create an empty list first
+for filename in glob.glob('C:/Users/laura/Documents/20210818 Python workshop/data/*.csv'):
+    cont=pd.read_csv(filename)
+    contents=cont.append(contents)
+    #print(filename,len(contents))
+
+contents = [] # create an empty list first
+for filename in glob.glob('C:/Users/laura/Documents/20210818 Python workshop/data/*.csv'):
+    contents[filename]=pd.read_csv(filename)
+
+cont.shape # gives dimensions of a data frame
+cont.shape[1] # gives number of columns
+cont.shape[0] # gives numer of rows
+
+# create our own functions
+def print_greeting():
+    print("hello!!!")
+
+print_greeting()
+
+# create a function that does something useful
+def print_date(year,month,day):
+    joined=str(year)+'-'+str(month)+'-'+str(day)
+    print(joined)
+    return(joined)
+
+Date = print_date(2021,"August",19)
+Date
+
+def average(values):
+    if len(values) == 0:
+        print("You must provide some values")
+        return None # None is a special type in python
+    return sum(values)/len(values)
+
+av = average([2,5,3])
+av = average([])
+print(av)
+
+def report(pressure):
+    print('pressure is',pressure) # this prints on the fly, but won't add this to a variable
+    return(pressure) # this will add this value to a variable if the function call is assigned to a variable
+print('calling', report(22.5))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
