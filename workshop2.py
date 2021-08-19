43b410ff513efbf5e9565dd3cd86d39055de3964
Laura Grogan <LauraGrogan@users.noreply.github.com>
2021-08-19 13:08:35 +1000

    added for loops examples

--- workshop2.py
+++ workshop2.py
@@ -27,3 +27,55 @@ empty = [1,"rrrrr",1.2345] # can't change strings in lists (immutable)
 del empty[1]
 print(empty)
 
+## For loops
+"I like cats"
+"I like dogs"
+"I like gold fish"
+"I like Jacob"
+
+subjects=["cats","dogs","gold fish","Jacob"]
+for i in subjects:
+    print("I really like", i)  # this is the iterator
+# can't comment on the first line of a for loop, but can add comments on the other lines. 
+
+for number in [2,3,4]:
+    print('the value of number * x=',number*2)
+
+primes = [2,3,5]
+for p in primes:
+    squared=p ** 2
+    cubed=p**3
+    print("original:",p,"squared:",squared,"cubed:",cubed)
+    
+squared = []
+primes = [2,3,5]
+for p in primes:
+    sqr=p**2
+    squared.append(sqr)
+print(squared)
+
+for i in range(0,4,1):
+    print("the value of i in this loop is:",i) # python ranges finish on the number before the one listed, ie, at 3. 
+    
+for i in range(0,len(primes),1):
+    print("the value of i in this loop is:",i)
+
+for i in range(0,len(primes),1):
+    print("the value of i in this loop is:",primes[i])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
